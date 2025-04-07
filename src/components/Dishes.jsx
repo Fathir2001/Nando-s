497f9f88f8e3d7ae2c915a0bd5c6bc6dfd4e2a98
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2025-04-07 18:14:50 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,11 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import img1 from "../assets/img/img1.jpg";
-import img2 from "../assets/img/img2.jpg";
-import img3 from "../assets/img/img3.jpg";
-import img4 from "../assets/img/img4.jpg";
-import img5 from "../assets/img/img5.jpg";
-import img6 from "../assets/img/img6.jpg";
+import dish1 from "../assets/img/dish1.jpg";
+import dish2 from "../assets/img/dish2.jpg";
+import dish3 from "../assets/img/dish3.jpg";
+import dish4 from "../assets/img/dish4.jpg";
+import dish5 from "../assets/img/dish5.jpg";
+import dish6 from "../assets/img/dish6.jpg";
+import dish7 from "../assets/img/dish7.jpg";
+import dish8 from "../assets/img/dish8.webp";
+import dish9 from "../assets/img/dish9.jpg";
+import dish10 from "../assets/img/dish10.webp";
+import dish11 from "../assets/img/dish11.webp";
+import dish12 from "../assets/img/dish12.jpeg";
 import DishesCard from "../layouts/DishesCard";
 
 const Dishes = () => {
@@ -16,12 +22,18 @@ const Dishes = () => {
       </h1>
 
       <div className=" flex flex-wrap gap-8 justify-center">
-        <DishesCard img={img1} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={img2} title="Tasty Dish" price="$12.99" />
-        <DishesCard img={img3} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={img4} title="Tasty Dish" price="$11.99" />
-        <DishesCard img={img5} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={img6} title="Tasty Dish" price="$12.99" />
+        <DishesCard img={dish1} title="Tasty Dish" price="$10.99" />
+        <DishesCard img={dish2} title="Tasty Dish" price="$12.99" />
+        <DishesCard img={dish3} title="Tasty Dish" price="$10.99" />
+        <DishesCard img={dish4} title="Tasty Dish" price="$11.99" />
+        <DishesCard img={dish5} title="Tasty Dish" price="$10.99" />
+        <DishesCard img={dish6} title="Tasty Dish" price="$12.99" />
+        <DishesCard img={dish7} title="Tasty Dish" price="$10.99" />
+        <DishesCard img={dish8} title="Tasty Dish" price="$12.99" />
+        <DishesCard img={dish9} title="Tasty Dish" price="$10.99" />
+        <DishesCard img={dish10} title="Tasty Dish" price="$11.99" />
+        <DishesCard img={dish11} title="Tasty Dish" price="$10.99" />
+        <DishesCard img={dish12} title="Tasty Dish" price="$12.99" />
       </div>
     </div>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import DishesCard from "../layouts/DishesCard";
-import menu1 from "../assets/img/menu1.jpg";
-import menu2 from "../assets/img/menu2.jpg";
-import menu3 from "../assets/img/menu3.jpg";
+import dish1 from "../assets/img/dish1.jpg";
+import dish2 from "../assets/img/dish2.jpg";
+import dish3 from "../assets/img/dish3.jpg";
+import dish4 from "../assets/img/dish4.jpg";
+import dish5 from "../assets/img/dish5.jpg";
+import dish6 from "../assets/img/dish6.webp";
+
 
 const Menu = () => {
   return (
@@ -12,9 +16,12 @@ const Menu = () => {
       </h1>
 
       <div className=" flex flex-wrap gap-8 justify-center">
-        <DishesCard img={menu1} title="Delicious Dish" price="$16.99" />
-        <DishesCard img={menu2} title="Delicious Dish" price="$18.99" />
-        <DishesCard img={menu3} title="Delicious Dish" price="$14.99" />
+        <DishesCard img={dish1} title="Delicious Dish" price="$16.99" />
+        <DishesCard img={dish2} title="Delicious Dish" price="$18.99" />
+        <DishesCard img={dish3} title="Delicious Dish" price="$14.99" />
+        <DishesCard img={dish4} title="Delicious Dish" price="$16.99" />
+        <DishesCard img={dish5} title="Delicious Dish" price="$18.99" />
+        <DishesCard img={dish6} title="Delicious Dish" price="$14.99" />
       </div>
     </div>
   );
