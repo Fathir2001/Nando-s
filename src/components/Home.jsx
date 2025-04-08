8935d63531b24897c8102a7491bc9a54a7336191
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2025-04-08 12:13:13 +0530

    Enhance Home component with framer-motion for improved animations and visual appeal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Button from "../layouts/Button";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-row justify-between items-center px-4 sm:px-8 md:px-16 lg:px-32 relative">
+    <div className="min-h-screen flex flex-row justify-between items-center px-4 sm:px-8 md:px-16 lg:px-32 relative overflow-hidden">
       {/* Mobile background image (hidden on medium screens and up) */}
       <div 
         className="absolute inset-0 bg-[url('./assets/img/PhoneBG.jpg')] bg-cover bg-no-repeat bg-center md:hidden"
@@ -20,18 +21,35 @@ const Home = () => {
       <div className="absolute inset-0 bg-black opacity-70 md:opacity-20 lg:opacity-10"></div>
       
       <div className="w-full lg:w-2/3 space-y-6 md:space-y-5 relative z-10">
-        <h1 className="text-white md:text-backgroundColor text-5xl sm:text-5xl md:text-5xl lg:text-6xl font-bold leading-tight">
+        <motion.h1 
+          className="text-white md:text-backgroundColor text-5xl sm:text-5xl md:text-5xl lg:text-6xl font-bold leading-tight"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
           Elevate Your Inner Foodie with Every Bite.
-        </h1>
-        <p className="text-white md:text-backgroundColor text-lg sm:text-lg md:text-lg max-w-xl font-medium leading-relaxed">
+        </motion.h1>
+
+        <motion.p 
+          className="text-white md:text-backgroundColor text-lg sm:text-lg md:text-lg max-w-xl font-medium leading-relaxed"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.5 }}
+        >
           From the moment you step in, our carefully curated ambience will make
           you feel right at home. Whether you're dining with family, friends, or
           colleagues, the warm, inviting atmosphere is the perfect backdrop for
           any occasion
-        </p>
-        <div className="pt-4 lg:pl-44">
+        </motion.p>
+
+        <motion.div 
+          className="pt-4 lg:pl-44"
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.8 }}
+        >
           <Button title="Order Now" />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
