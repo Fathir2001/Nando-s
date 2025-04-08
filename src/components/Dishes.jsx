b177b2066b767b95f77310375f5e9ceca8eec99b
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2025-04-08 11:22:08 +0530

    update Dishes component to display correct dish names and prices

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -22,18 +22,18 @@ const Dishes = () => {
       </h1>
 
       <div className=" flex flex-wrap gap-8 justify-center">
-        <DishesCard img={dish1} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={dish2} title="Tasty Dish" price="$12.99" />
-        <DishesCard img={dish3} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={dish4} title="Tasty Dish" price="$11.99" />
-        <DishesCard img={dish5} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={dish6} title="Tasty Dish" price="$12.99" />
-        <DishesCard img={dish7} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={dish8} title="Tasty Dish" price="$12.99" />
-        <DishesCard img={dish9} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={dish10} title="Tasty Dish" price="$11.99" />
-        <DishesCard img={dish11} title="Tasty Dish" price="$10.99" />
-        <DishesCard img={dish12} title="Tasty Dish" price="$12.99" />
+        <DishesCard img={dish1} title="BBQ" price="LKR 700" />
+        <DishesCard img={dish2} title="Chicken Submarine" price="LKR 650" />
+        <DishesCard img={dish3} title="Chicken Burger" price="LKR 550" />
+        <DishesCard img={dish4} title="Crispy Chicken Burger" price="LKR 700" />
+        <DishesCard img={dish5} title="Chicken Fried Rice" price="LKR 850" />
+        <DishesCard img={dish6} title="Beef Fried Rice" price="LKR 900" />
+        <DishesCard img={dish7} title="Egg Fried Rice" price="LKR 700" />
+        <DishesCard img={dish8} title="Chicken Noodles" price="LKR 850" />
+        <DishesCard img={dish9} title="Beef Noodles" price="LKR 950" />
+        <DishesCard img={dish10} title="Egg Noodles" price="LKR 700" />
+        <DishesCard img={dish11} title="Chicken Dolphin" price="LKR 900" />
+        <DishesCard img={dish12} title="Beef Dolphin" price="LKR 900" />
       </div>
     </div>
   );
